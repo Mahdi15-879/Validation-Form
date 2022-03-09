c3309c43c5b73f8ac37fe72c1a2b5537d5b0a155
Mahdi-Heydari <mahdiheydari15879@gmail.com>
2022-03-09 12:35:32 +0330

    Develope SignUp component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import React from "react";
+
+import SignUp from "./components/SignUp";
+
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <SignUp />
+    </div>
+  );
 }
 
 export default App;
